014d291de4dc905b657a289eb26dcfba693cfc91
Maxim Khan-Magomedov <maxim.km@gmail.com>
2021-09-10 08:58:32 +0300

    Commented amends by default

--- db/migrate/amends/20210816060606_create_oembed_receivers.rb
+++ db/migrate/amends/20210816060606_create_oembed_receivers.rb
@@ -3,19 +3,19 @@
 # Create tables for OEmbed
 class CreateOembedReceivers < ActiveRecord::Migration[6.1]
   def change
-    create_table :oembed_receivers, comment: 'Receivers for OEmbed content' do |t|
-      t.string :slug, null: false, index: true
-    end
-
-    create_table :oembed_domains, comment: 'Supported domains for OEmbed' do |t|
-      t.references :oembed_receiver, null: false, foreign_key: { on_update: :cascade, on_delete: :cascade }
-      t.string :name, null: false, index: true
-    end
-
-    create_table :oembed_links, comment: 'Embedded links' do |t|
-      t.string :url, null: false
-      t.text :code
-      t.timestamps
-    end
+    # create_table :oembed_receivers, comment: 'Receivers for OEmbed content' do |t|
+    #   t.string :slug, null: false, index: true
+    # end
+    #
+    # create_table :oembed_domains, comment: 'Supported domains for OEmbed' do |t|
+    #   t.references :oembed_receiver, null: false, foreign_key: { on_update: :cascade, on_delete: :cascade }
+    #   t.string :name, null: false, index: true
+    # end
+    #
+    # create_table :oembed_links, comment: 'Embedded links' do |t|
+    #   t.string :url, null: false
+    #   t.text :code
+    #   t.timestamps
+    # end
   end
 end
--- db/migrate/amends/20210907070707_add_checksum_to_simple_images.rb
+++ db/migrate/amends/20210907070707_add_checksum_to_simple_images.rb
@@ -3,11 +3,11 @@
 # Add checksum field to simple images
 class AddChecksumToSimpleImages < ActiveRecord::Migration[6.1]
   def up
-    add_column :simple_images, :checksum, :string, index: true
-    SimpleImage.order(:id).each(&:save)
+    # add_column :simple_images, :checksum, :string, index: true
+    # SimpleImage.order(:id).each(&:save)
   end
 
   def down
-    remove_column :simple_images, :checksum
+    # remove_column :simple_images, :checksum
   end
 end
