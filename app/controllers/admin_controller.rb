8f8997aa8d3455b3ab11d65e23003e99db0f477a
Maxim Khan-Magomedov <maxim.km@gmail.com>
2021-10-27 09:57:03 +0300

    Extracted access restriction

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,29 +2,7 @@
 
 # Common administrative controller
 class AdminController < ApplicationController
+  include RestrictedAccess
+
   before_action :restrict_access
-
-  private
-
-  def restrict_access
-    user_action = role_end_from_action
-    role_name = "#{component_handler.slug}.#{user_action}"
-    error = t('admin.errors.unauthorized.missing_role', role: role_name)
-
-    handle_http_401(error) unless component_handler.permit?(user_action)
-  end
-
-  def role_end_from_action
-    role = action_to_role_map.select { |k| k.include?(action_name) }.values.last
-    role || 'default'
-  end
-
-  def action_to_role_map
-    view = %w[index show search]
-    edit = %w[create destroy edit new priority toggle update]
-    {
-      view => "#{controller_name}.view",
-      edit => "#{controller_name}.edit"
-    }
-  end
 end
--- lib/biovision/version.rb
+++ lib/biovision/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Biovision
-  VERSION = '0.9.211008.1'
+  VERSION = '0.9.211027.0'
 end
